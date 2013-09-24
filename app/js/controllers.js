707b8c39bca1eb4ffd12b48bdd0ecae1577c3760
Ivan Kubinyi <ivan.kubinyi@metropolia.fi>
2013-09-24 13:43:05 +0300

    WIP: ONIX message header added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,17 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('MessageCtrl', ['$scope', function($scope) {
+  	$scope.master = {};
+
+  	$scope.update = function(message) {
+  		$scope.master = angular.copy(message);
+  	}
+
+  	$scope.reset = function () {
+  		$scope.message = angular.copy($scope.master);
+  	}
+
+  	$scope.reset();
   }]);
