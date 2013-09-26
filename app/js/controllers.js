b2b25e23e391dc51648615be5b7af3788f172d8f
Ivan Kubinyi <ivan.kubinyi@metropolia.fi>
2013-09-26 14:58:51 +0300

    Options for select aer built from JSON files

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,8 +9,43 @@ angular.module('myApp.controllers', ['ui.bootstrap']).
   .controller('MyCtrl2', [function() {
 
   }])
-  .controller('MessageCtrl', ['$scope', function($scope) {
-  	$scope.master = {};
+
+  .controller('MessageCtrl', ['$scope','$http', function($scope, $http) {
+
+  	$scope.master = {
+  		products: [
+  			{}
+  		]
+  	};
+
+  	// Get the list for the forms
+  	$scope.productNotificationTypeList = {};
+  	$http.get('assets/lists/list1.json').success(function(data){
+  		$scope.productNotificationTypeList = data;
+  	});
+
+  	$scope.productIdTypeList = {};
+  	$http.get('assets/lists/list5.json').success(function(data){
+  		$scope.productIdTypeList = data;
+  	});
+
+  	$scope.productFormList = {};
+  	$http.get('assets/lists/list7.json').success(function(data){
+  		$scope.productFormList = data;
+  	});
+
+  	$scope.addProduct = function() {
+  		$scope.message.products.push({type:'', value:''});
+  	}
+
+  	$scope.removeProduct = function(product) {
+  		var products = $scope.message.products;
+  		for (var i = 0, ii = products.length; i < ii; i++) {
+  			if (product === products[i]) {
+  				products.splice(i, 1);
+  			}
+  		}
+  	}
 
   	$scope.update = function(message) {
   		$scope.master = angular.copy(message);
@@ -22,6 +57,7 @@ angular.module('myApp.controllers', ['ui.bootstrap']).
 
   	$scope.reset();
   }])
+
   .controller('DatepickerCtrl', ['$scope', '$timeout', function($scope, $timeout) {
   	 $scope.today = function() {
   	 	$scope.message.sentDate = new Date();
@@ -44,5 +80,7 @@ angular.module('myApp.controllers', ['ui.bootstrap']).
 	 	'year-format': "'yy'",
 	    'starting-day': 1
 	 };
-
+  }])
+  .controller('TimepickerCtrl', ['$scope', function($scope) {
+  	$scope.message.sentTime = new Date();
   }]);
